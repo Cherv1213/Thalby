c2b9f49d433bddfcb1aedf5f57accf89a1218afe
Cherv1213 <vlad70052@gmail.com>
2023-02-09 18:21:30 +0200

    234234

--- script/main.js
+++ script/main.js
@@ -2,10 +2,28 @@
 
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
-if(iconMenu){
-   iconMenu.addEventListener("click", function (e){
-      document.body.classList.toggle('lock');
+const list = document.querySelector('.menu__list');
+
+burger();
+
+function burger(){
+   iconMenu.addEventListener('click',(e)=>{
+   
       iconMenu.classList.toggle('active');
-      menuBody.classList.toggle('active');
+   
+       if(menuBody.style.maxHeight){
+         menuBody.style.maxHeight = null;
+        } else {
+         menuBody.style.maxHeight = 140 + 'px';
+        };
+   
+         document.addEventListener('click',(el)=>{
+         const click = el.composedPath().includes(iconMenu) || el.composedPath().includes(list);
+   
+         if(!click){
+          menuBody.style.maxHeight = null;
+          iconMenu.classList.remove('active');
+         }
+       })
    });
 }
