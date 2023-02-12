e10638686f3a24fc8d727d44a36591f27d151b36
Cherv1213 <vlad70052@gmail.com>
2023-02-12 18:09:41 +0200

    234234

--- script/main.js
+++ script/main.js
@@ -27,3 +27,40 @@ function burger(){
        })
    });
 }
+
+const offset = 100;
+const scrollUp = document.querySelector('.scroll-up');
+const scrollUpSvgPath = document.querySelector('.scroll-up__svg-path');
+const pathLength = scrollUpSvgPath.getTotalLength();
+ 
+scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+scrollUpSvgPath.style.transition = 'stroke-dashoffset 20ms';
+
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+//
+const updateDashoffset = () => {
+   const height = document.documentElement.scrollHeight - window.innerHeight;
+   const dashoffset = pathLength - (getTop() * pathLength / height);
+
+   scrollUpSvgPath.style.strokeDashoffset = dashoffset;
+};
+
+//
+window.addEventListener('scroll', () =>{
+   updateDashoffset();
+
+   if(getTop() > offset) {
+      scrollUp.classList.add('_active');
+   } else {
+      scrollUp.classList.remove('_active');
+   }
+});
+
+//
+scrollUp.addEventListener('click', () =>{
+   window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+   });
+});
